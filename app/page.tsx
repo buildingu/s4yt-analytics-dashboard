c57d056cf6cecd4053dcd77b9004559b8bc8c3c8
M.S.T.O.P. <mstop4@gmail.com>
2026-02-04 22:54:47 -0500

    add revalidate config to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { analyzeUsers, convertToChartData } from '@/lib/analytics';
 import Home from './Home';
+export const revalidate = 60;
 
 export default async function Page() {
   const stats = await analyzeUsers();
